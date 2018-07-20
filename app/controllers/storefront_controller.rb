7e4d07531e0682c05363cb25b69d21805c8cf443
Jacob Garcia <janikgar@gmail.com>
2018-07-19 22:47:28 -0500

    added products category/brand pages, made Devise more robust

--- app/controllers/storefront_controller.rb
+++ app/controllers/storefront_controller.rb
@@ -1,13 +1,27 @@
 class StorefrontController < ApplicationController
+
   def all_items
   	@products = Product.all
   end
 
   def items_by_category
-  	@products = Product.all
+    # @products = Product.all
+    # @categories = Product.all.group(:category).pluck(:category)
+    @categories = Category.joins(:products).order(name: :asc)
+    @products_by_category = Array.new
+    @categories.each do |category|
+      category_products =  Product.all.where(category_id: category.id)
+      @products_by_category.push({category.name => category_products.to_a})
+    end
   end
 
   def items_by_brand
-  	@products = Product.all
+    # @products = Product.all
+    @brands = Product.all.group(:brand).pluck(:brand)
+    @products_by_brand = []
+    @brands.each do |brand|
+      brand_products = Product.all.where(brand: brand)
+      @products_by_brand.push({brand => brand_products.to_a})
+    end
   end
 end
---
+++ db/migrate/20180720012653_add_fields_to_users.rb
@@ -0,0 +1,11 @@
+class AddFieldsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :street_address, :string
+    add_column :users, :street_address_2, :string
+    add_column :users, :city, :string
+    add_column :users, :state, :string
+    add_column :users, :zip, :string
+  end
+end
